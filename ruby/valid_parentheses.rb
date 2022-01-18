4d76c6027fe044900bdcc4970afb9b3af58ca9dd
siddushar <siddushar@gmail.com>
2022-01-18 20:08:42 +0530

    Create valid_parentheses.rb

---
+++ ruby/valid_parentheses.rb
@@ -0,0 +1,16 @@
+def is_valid(s)
+  stack = []
+  s.each_char do |c|
+    case c
+      when '(', '{', '['
+        stack.push(c)
+      when ')'
+        return false if stack.pop() != '('
+      when ']'
+        return false if stack.pop() != '('
+      when '}'
+        return false if stack.pop() != '('
+      end
+  end
+  stack.empty?
+end
